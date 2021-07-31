7093b9a43828d326b6c8b653773d91d5170b5260
khaled al shishani <khaled.shishani32@gmail.com>
2021-07-31 17:51:20 +0300

    change the code

--- array-binary/binary.py
+++ array-binary/binary.py
@@ -1,29 +1,40 @@
 
 
-def BinarySearch(my_list,target):
-  for i in my_list :
-    meddle_index =int((0+len(my_list)-1)/2)
-    if target==my_list[meddle_index] :
-        print(meddle_index)
-        break
-    if target>my_list[meddle_index]:
-       meddle_index=int((meddle_index+len(my_list)-1)/2)
-    elif target==my_list[meddle_index] :
-        print(meddle_index)
-        break
-    if target< my_list[meddle_index]:
-       meddle_index=int(meddle_index/2)
-    if target==my_list[meddle_index] :
-        print(meddle_index)
-        break
-    else :
-        print(-1)
-        break
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+ 
+    while low <= high:
+ 
+        mid = (high + low) // 2
+ 
+        if arr[mid] < x:
+            low = mid + 1
 
-BinarySearch([4, 8, 15, 16, 23, 42], 15)
-BinarySearch([-131, -82, 0, 27, 42, 68, 179], 42)
-BinarySearch([11, 22, 33, 44, 55, 66, 77], 90)
-BinarySearch([1, 2, 3, 5, 6, 7], 4)
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+ 
+   
+    return -1
+ 
+ 
+
+# arr = [ 2, 3, 4, 10, 40 ]
+# x = 10
+ 
+final= binary_search([4, 8, 15, 16, 23, 42], 15)
+
+if final != -1:
+    print("Element is present at index", str(final))
+else:
+    print("Element is not present in array")
+
+
+
+
 
 
 
