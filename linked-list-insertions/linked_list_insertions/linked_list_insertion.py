d961b07c80fca8dfece2d468be321a0331865829
khaled al shishani <khaled.shishani32@gmail.com>
2021-08-13 19:32:58 +0300

    added a whiteboard

--- linked-list-insertions/linked_list_insertions/linked_list_insertion.py
+++ linked-list-insertions/linked_list_insertions/linked_list_insertion.py
@@ -33,25 +33,7 @@ class LinkedList:
  
  
 
-def zipLists(head1, head2):
- 
-    temp = None
- 
-    if head1 is None:
-        return head2
- 
-    if head2 is None:
-        return head1
- 
-    if head1.data <= head2.data:
-        temp = head1
-        temp.next = zipLists(head1.next, head2)
-         
-    else:
-        temp = head2
-        temp.next = zipLists(head1, head2.next)
 
-    return temp
  
  
 
