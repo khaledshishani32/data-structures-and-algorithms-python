101d8f2e62f2dae3aa571f512687faebd8d59c9c
khaled al shishani <khaled.shishani32@gmail.com>
2021-08-14 17:11:30 +0300

    finish code challenge 11 and added readme , whiteboard

---
+++ stack-queue-pseudo/README.md
@@ -0,0 +1,9 @@
+## Challenge Summary 
+
+### Create a new class called pseudo queue by create two instance from stack class and use the stack method to implementation  the , enqueue and dequeue methods
+
+
+## whiteboard
+![stack-queue-pseudo](PseudoQueue.jpg)
+
+
---
+++ stack-queue-pseudo/stack_queue_pseudo/stack-queue-pseudo.py
@@ -0,0 +1,83 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, data):
+        node = Node(data)
+        if self.top == None:
+            self.top = node 
+        else:
+            node.next = self.top
+            self.top = node
+
+    def pop(self):
+        if not self.is_empty():
+            temp = self.top
+            self.top = self.top.next
+            temp.next = None
+            return temp.data
+        else:
+            return "This is Empty stack"
+
+    def is_empty(self):
+        if self.top == None:
+            return True
+        else:
+            return False
+
+    def peek(self):
+        if self.is_empty():
+            raise Exception("The satck empty cannot peek !")
+        return self.top.data
+ 
+class PseudoQueue :
+    def __init__(self):
+        self.first_s=Stack()
+        self.second_s=Stack()
+        self.rear=None
+        self.front=None
+
+    def enqueue(self , data):
+        self.first_s.push(data)
+        self.rear=self.first_s.top.data
+   
+
+    def dequeue(self):
+        if self.first_s.top :
+            temp = self.first_s
+            while not temp.is_empty():
+                self.secand_stack.push(temp.pop())
+
+            poped = self.secand_stack.pop()
+            self.front = self.secand_stack.top
+            self.first_s = Stack()
+            temp2 = self.secand_stack
+            while not temp2.is_empty():
+                self.first_s.push(temp2.pop())
+            return poped
+
+    def desplay(self):
+        queue_str=''
+        temp = self.first_s.top
+        while temp:
+            queue_str+= '(' +str(temp.data)+ ')'+ '-->'
+            temp=temp.next
+        print(queue_str + "None")
+ 
+
+if __name__== "__main__" :
+    queue= PseudoQueue()
+    queue.enqueue(10)
+    queue.enqueue(15)
+    queue.enqueue(20)
+
+    queue.enqueue(50)
+    queue.desplay()
