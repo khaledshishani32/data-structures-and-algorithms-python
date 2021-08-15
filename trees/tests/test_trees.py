5d3102d84dabd5bdd1190874366fdec19c4eca74
khaled al shishani <khaled.shishani32@gmail.com>
2021-08-15 20:28:16 +0300

    added test

--- trees/tests/test_trees.py
+++ trees/tests/test_trees.py
@@ -1,5 +1,51 @@
 from trees import __version__
 
+from trees.tree import *  
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_instantiate_empty_tree():
+   tree = Binary_s_tree()
+
+   assert tree.data==None
+
+
+def test_single_root():
+ tree = Binary_s_tree(10)
+
+ assert tree.data==10
+
+def test_left_right_single_root():
+ tree = Binary_s_tree(10)
+ tree.insert(7)
+ tree.insert(15)
+
+ assert tree.data==10
+ assert tree.right.data==15
+ assert tree.left.data==7
+
+
+# def test_preorder():
+#  tree = Binary_s_tree(10)
+#  tree.insert(15)
+#  tree.insert(20)
+#  tree.insert(13)
+#  tree.insert(7)
+#  tree.insert(4)
+#  tree.insert(9)
+ 
+#  assert  tree.preorder() =='10-->7-->4-->9-->15-->13-->20-->'
+
+
+def test_inorder():
+ tree = Binary_s_tree(10)
+ tree.insert(15)
+ tree.insert(20)
+ tree.insert(13)
+ tree.insert(7)
+ tree.insert(4)
+ tree.insert(9)
+ 
+ assert  tree.inorder() =='10-->7-->4-->9-->15-->13-->20-->'
--- trees/trees/tree.py
+++ trees/trees/tree.py
@@ -1,75 +1,100 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+class Binary_s_tree:
+    def __init__(self,data=None,left=None,right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+        self.size = 0 
+     
+    def insert(self,data):
+        if self.data == data:
+            return False #     duplicate data
+        elif self.data > data:
+            if self.left is not None:
+                return self.left.insert(data)
+                self.size+=1
+            else:
+                self.left=Binary_s_tree(data)
+                self.size+=1
+                return True
+        else:
+            if self.right is not None:
+                self.size+=1
+                return self.right.insert(data)
+            else:
+                 self.right = Binary_s_tree(data)
+                 self.size+=1
+                 return True      
 
+    def find(self, data):
+        if self.data == data:
+            return True
+        elif self.data > data:
+            if self.left is None:
+                return False
+            else:
+                return self.left.find(data) # 
+        elif self.data < data:
+            if self.right is None:
+                return False
+            else:
+                return self.right.find(data)
 
-
-class Binary_tree:
-    def __init__(self):
-        self.root = None
-
-    def pre_order(self):
-
-        values = ''
-        if not self.root:
-            return 'The Tree is empty'
-
-        def traverse(node):
-            nonlocal values 
-            values += str(node.value) 
-
-            if node.left:
-                traverse(node.left)
-
-            if node.right:
-                traverse(node.right)
-                  
-        traverse(self.root)
-
-        return values
+    def preorder(self):
+        if self is not None:
+            print(self.data ,end="-->")
+            if self.left is not None:
+                self.left.preorder()
+            
+            if self.right is not None:
+                self.right.preorder()
     
-    def post_order(self):
-        values = ''
-        if not self.root:
-            return 'The Tree is empty'
-        
-        def traverse(node):
-            nonlocal values 
+    def inorder(self):
+        if self is not None:
+            if self.left is not None:
+                self.left.inorder()
+            print(self.data ,end="-->")
+            if self.right is not None:
+                self.right.inorder()        
+    
+    def postorder(self):
+        if self is not None:
+            if self.left is not None:
+                self.left.postorder()
+            
+            if self.right is not None:
+                self.right.postorder() 
+            print(self.data , end=" ")
+            return
+    
+    def is_empty(self):
+        return self.size == 0
+    
+    def __str__(self):
+        return self.data
+    
 
-            if node.left:
-                
-                traverse(node.left)
-                
-            if node.right:
-                traverse(node.right)
 
-            values += str(node.value)       
 
-        
-        traverse(self.root)
-         
-        return values
+# if __name__=="main":
+tree = Binary_s_tree(10)
+tree.insert(15)
+# # for i in [15,10,2,12,3,1,13,6,11,4,14,9]:
+# #     tree.insert(i)
+# # for i in range(16):
+# #     print(tree.find(i) , end=" ")
+tree.insert(20)
+tree.insert(13)
+tree.insert(7)
+tree.insert(4)
+tree.insert(9)
 
-    def in_order(self):
-        values = ''
-        if not self.root:
-            return 'The Tree is empty'
-        
-        def traverse(node):
-            nonlocal values 
+# print(tree.right.left.data)
+# # print(tree.right.right.data)
 
-            if node.left:
-                
-                traverse(node.left)
-
-            values += str(node.value)
-                
-            if node.right:
-                traverse(node.right)
-
-        traverse(self.root)
-         
-        return values
-
+# print(tree.find(10))            
+# print(tree.preorder())
+tree.inorder()
+print()
+# print(tree.postorder())
+# print(tree.is)
+# print(tree.__str__())
