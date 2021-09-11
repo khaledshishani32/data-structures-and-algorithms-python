c835272e008b17d24d5307477d50db25e972010a
khaled al shishani <khaled.shishani32@gmail.com>
2021-09-11 15:55:35 +0300

    finish  challenge 32

---
+++ hashmap-tree-intersection/README.md
@@ -0,0 +1,7 @@
+# Challenge Summary
+write a function that that take two tree and return the intersection between them  .
+## Whiteboard Process
+![](intersection.jpg)
+## Approach & Efficiency
+time--> o(n)
+space--> o(1)
--- hashmap-tree-intersection/tests/test_hashmap_tree_intersection.py
+++ hashmap-tree-intersection/tests/test_hashmap_tree_intersection.py
@@ -1,5 +1,32 @@
 from hashmap_tree_intersection import __version__
 
+from hashmap_tree_intersection.hashmap_tree_intersection import *
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+
+
+def test_hashmap_tree_intersection():
+    tree1 = B_tree()
+    tree1.insert(10)
+    tree1.insert(20)
+    tree1.insert(30)
+    tree1.insert(40)
+    tree1.insert(50)
+    tree1.insert(600)
+    
+    
+    tree2 = B_tree()
+    tree2.insert(10)
+    tree2.insert(20)
+    tree2.insert(30)
+    tree2.insert(40)
+    tree2.insert(50)
+    tree2.insert(7)
+   
+    actual = intersection(tree1 , tree2)
+    excepted = [10, 20, 30, 40, 50]
+
+    assert actual == excepted
