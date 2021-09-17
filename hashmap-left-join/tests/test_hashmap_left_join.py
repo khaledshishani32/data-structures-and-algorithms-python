58148294265774c13eae4023f177d9f257f9bb36
khaled al shishani <khaled.shishani32@gmail.com>
2021-09-17 17:49:38 +0300

    finish cc33

--- hashmap-left-join/tests/test_hashmap_left_join.py
+++ hashmap-left-join/tests/test_hashmap_left_join.py
@@ -1,5 +1,28 @@
 from hashmap_left_join import __version__
 
-
+from hashmap_left_join.hashmap_left_join import *
 def test_version():
     assert __version__ == '0.1.0'
+
+
+
+def test_join_left():
+    hash_one = HashTable()
+
+    hash_one.add('fond', 'enamored')
+    hash_one.add('wrath', 'anger')
+    hash_one.add('diligent', 'employed')
+    hash_one.add('outfit', 'grap')
+    hash_one.add('guide', 'usher')
+
+    hash_two = HashTable()
+
+    hash_two.add('fond', 'averse')
+    hash_two.add('wrath', 'delight')
+    hash_two.add('diligent', 'idle')
+    hash_two.add('guide', 'follow')
+    hash_two.add('flow', 'jam')
+    
+    actual = hashmap_left_join(hash_one,hash_two)
+    excepted = [['guide', 'usher', 'follow'], ['wrath', 'anger', 'delight'], ['diligent', 'employed', 'idle'], ['fond', 'enamored', 'averse'], ['outfit', 'grap', 'NULL']]
+    assert actual == excepted
